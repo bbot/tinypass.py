e8441b431f0dfd23ba00a0e4922fcc19a24ea8fc
bbot <bbot@bbot.org>
2014-01-12 19:55:28 -0800

    second commit

---
+++ tinypass.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+"""
+tinypass.py - Minimal, very low security file authentication app
+
+Checks unsigned cookies against unsalted passwords stored on disk
+in plaintext; to serve large static files.
+
+Requires bottle.py
+
+Written by bbot@bbot.org
+This is free and unencumbered software released into the public domain.
+"""
+
+from bottle import route, run, request, abort
+import pickle
+
+# This is where the static files you're serving should live.
+#HTROOT = '/srv/www/'
+
+# Or you can import them from an external module, if you're being fancy
+from constants import HTROOT
+
+@route('/<filename>')
+def index(filename):
+    username = request.get_cookie("username")
+    password = request.get_cookie("password")
+    if not username:
+        return "login form\n";
+    elif (check_password(filename, username, password)):
+        return "match\n";
+#        return static_file(file, root=HTROOT)
+    else:
+        return "bad username or password\n";
+
+def check_password(filename, username, password):
+    pkl_file = open('data.pkl', 'rb')
+    data = pickle.load(pkl_file)
+    try:
+        row = data[filename]
+    except KeyError:
+        abort(404, "File not found")
+    if (row['username'] == username and row['password'] == password):
+        return True
+    else:
+        return False
+
+run(host='localhost', port=8080, debug=True)
+
